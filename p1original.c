7b1405892f0fd417f6394122594eb3125f62e5e5
Arudhra Chandrasekaran <94979081+arudhra-techy963@users.noreply.github.com>
2022-02-02 06:37:57 +0000

    Adding 2 numbers pass by ref

--- p1original.c
+++ p1original.c
@@ -1,2 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+int input();
+int add(int x, int y, int* result);
+void output(int x, int y, int result);
+
+int main()
+{
+    int a,b;
+    a = input();
+    b = input();
+
+    int sum;
+    add(a, b, &sum);
+
+    output(a,b, sum);
+
+    return 0;
+
+}
+
+int input(int x)
+{
+    printf("Enter number = ");
+    scanf("%d", &x);
+
+    return x;
+}
+
+int add(int x, int y, int* result)
+{
+    *result = x + y;
+
+    return *result ;
+}
+
+void output(int x, int y, int result)
+{
+    printf("\nThe answer: \n%d + %d = %d",x,y,result);
 }
